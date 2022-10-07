e87039fd79535aaca9eed521ce2df5e12e35d9c0
Takumaron <tennis.takumax@gmail.com>
2022-10-07 12:47:19 +0900

    add variable

---
+++ src/main/kotlin/variable.kt
@@ -0,0 +1,15 @@
+fun main() {
+    /**
+     * 変数
+     * val: 定数宣言（immutable）
+     * var: 変数宣言（mutable）
+     */
+    val id = 1 // valは変更不可
+    var name = "Taro" // varは変更可能
+    name = "Jiro"
+
+    /**
+     * 変数展開
+     */
+    println("id: $id, name: $name")
+}
