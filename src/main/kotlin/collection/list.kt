bf8de7958e594b7f453869841d02930359e81215
Takumaron <tennis.takumax@gmail.com>
2022-10-07 13:35:23 +0900

    add list

---
+++ src/main/kotlin/collection/list.kt
@@ -0,0 +1,25 @@
+package collection
+
+fun main() {
+    /**
+     * listOf
+     * 変更不可（immutable）
+     */
+    val list: List<Int> = listOf<Int>(1, 2, 3, 4, 5)
+    println(list)
+    println(list[2])
+
+    // ショートハンド
+    val listShortHand = listOf("this", "is", "shorthand")
+    println(listShortHand)
+
+    /**
+     * mutableListOf
+     * 変更可能（mutable）
+     */
+    val mutableList: MutableList<Int> = mutableListOf<Int>(1, 2, 3, 4, 5)
+    mutableList.add(6)
+    println(mutableList)
+    mutableList.removeAt(0)
+    println(mutableList.size)
+}
